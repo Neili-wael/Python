041ace97d6bde3fcebd7cdcf2cfcce4730dbf8f0
Neili Wael <Neiliwael9@gmail.com>
2024-03-21 00:22:52 +0100

    scrape value into new file

--- login_automated.py
+++ login_automated.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import datetime
 from selenium.webdriver.common.by import By
 
 def get_driver():
@@ -16,6 +17,10 @@ def get_driver():
     driver = webdriver.Chrome(options=options)
     driver.get("https://automated.pythonanywhere.com/login/")
     return driver
+def clean_text(text):
+    #Extract only the number
+    output = int(text.split(": ")[1])
+    return output
 
 def main():
     driver = get_driver()
@@ -29,4 +34,18 @@ def main():
     time.sleep(3)
     driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
     time.sleep(10)
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name1 = f"file_{current_datetime}.txt"
+    element =driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    element3 = str(clean_text(element.text))
+    with open(file_name1, 'w') as f:
+        f.write(element3)
+    time.sleep(2)
+    current_datetime2 = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    element1 = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    element2 = str(clean_text(element1.text))
+    file_name2 = f"file_{current_datetime2}.txt"
+    with open(file_name2, 'w') as f1:
+        f1.write(element2)
+
 print(main())
